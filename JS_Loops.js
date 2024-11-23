9b4d5473a9a0e9ec36078190beef816a8cd7596a
Jenil Savalia <jenil.savalia.cd@gmail.com>
2024-11-23 15:53:41 +0530

    loops notes

---
+++ JS_Loops.js
@@ -0,0 +1,11 @@
+//  for loop 
+//  for in loop (iterat over property(KEY Value) of object)
+// for of loop  (iterate throuth values of object)
+// while loop (run's till condotion is true)
+// do while loop (varient of while loop but runs code once before checking condition )
+// 
+
+
+
+// The do while loop is a variant of the while loop. This loop will execute the code block once, before checking if the condition is true, then it will repeat the loop as long as the condition is true.
+
